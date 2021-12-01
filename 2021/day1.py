1d8d51934cfa6c39c2cb5226408fe1bb327cc677
sergiogragera <sergiogragera@gmail.com>
2021-12-01 10:30:28 +0100

    fix: extract count increasements definition to use in part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -4,12 +4,12 @@ def is_increasing(a, b):
     return int(a) < int(b)
 
 
-def get_increasements(measurements):
-    sum([is_increasing(measurements[i], measurements[i + 1])
+def count_increasements(measurements):
+    return sum([is_increasing(measurements[i], measurements[i + 1])
          for i in range(len(measurements) - 1)])
 
 
 with open('2021/resources/day1.1.txt', 'r') as file:
     measurements = file.readlines()
-    print('There are {} measurements that are larger than previous measurement'.format(
-        get_increasements(measurements)))
+    print('Part 1: there are {} measurements that are larger than previous measurement'.format(
+        count_increasements(measurements)))
