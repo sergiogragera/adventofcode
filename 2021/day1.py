83d028cc5ca351e199d1531d1e5110c9dba67a23
sergiogragera <sergiogragera@gmail.com>
2021-12-01 10:31:12 +0100

    feat: add part 2 count three measurements tree

--- 2021/day1.py
+++ 2021/day1.py
@@ -6,10 +6,23 @@ def is_increasing(a, b):
 
 def count_increasements(measurements):
     return sum([is_increasing(measurements[i], measurements[i + 1])
-         for i in range(len(measurements) - 1)])
+                for i in range(len(measurements) - 1)])
 
 
 with open('2021/resources/day1.1.txt', 'r') as file:
     measurements = file.readlines()
     print('Part 1: there are {} measurements that are larger than previous measurement'.format(
+        count_increasements(measurements)))
+
+# https://adventofcode.com/2021/day/1#part2
+
+
+def get_three_measurments(measurements):
+    return [int(measurements[i]) + int(measurements[i + 1]) + int(measurements[i + 2])
+            for i in range(len(measurements) - 2)]
+
+
+with open('2021/resources/day1.2.txt', 'r') as file:
+    measurements = get_three_measurments(file.readlines())
+    print('Part 2: there are {} measurements that are larger than previous measurement'.format(
         count_increasements(measurements)))
