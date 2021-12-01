0987099df0e6010049a8cbc521ba79355ecf8ec6
sergiogragera <sergiogragera@gmail.com>
2021-12-01 10:10:52 +0100

    feat: add solver to day 1 step 1

---
+++ 2021/day1.py
@@ -0,0 +1,11 @@
+# https://adventofcode.com/2021/day/1
+
+def is_increasing(a, b):
+    return int(a) <= int(b)
+
+
+with open('2021/resources/day1.txt', 'r') as file:
+    measurements = file.readlines()
+    increases = sum([is_increasing(measurements[i], measurements[i + 1])
+                    for i in range(len(measurements) - 1)])
+    print('There are {} measurements that are larger than previous measurement'.format(increases))
