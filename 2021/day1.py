0b59e6a128f094d44951e0c74def11c409736fe0
sergiogragera <sergiogragera@gmail.com>
2021-12-01 10:25:48 +0100

    refactor: extract count increasements definition to use in part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -4,8 +4,12 @@ def is_increasing(a, b):
     return int(a) < int(b)
 
 
+def get_increasements(measurements):
+    sum([is_increasing(measurements[i], measurements[i + 1])
+         for i in range(len(measurements) - 1)])
+
+
 with open('2021/resources/day1.1.txt', 'r') as file:
     measurements = file.readlines()
-    increases = sum([is_increasing(measurements[i], measurements[i + 1])
-                    for i in range(len(measurements) - 1)])
-    print('There are {} measurements that are larger than previous measurement'.format(increases))
+    print('There are {} measurements that are larger than previous measurement'.format(
+        get_increasements(measurements)))
