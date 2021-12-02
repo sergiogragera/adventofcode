2ac7bc1a257f1c6a80f039cf0145d3727bfc5f6c
sergiogragera <sergiogragera@gmail.com>
2021-12-02 08:09:54 +0100

    feat: solve problem from day 2 part 2

--- 2021/day2.js
+++ 2021/day2.js
@@ -38,3 +38,38 @@ readFile('2021/resources/day2.1.txt').split('\n').forEach(line => {
 });
 
 console.info(submarine.getResult());
+
+
+// https://adventofcode.com/2021/day/2#part2
+
+class Submarine2 extends Submarine {
+    constructor() {
+        super();
+        this.aim = 0;
+    }
+
+    down(y) {
+        //super.down(y);
+        this.aim += y;
+    }
+
+    up(y) {
+        //super.up(y);
+        this.aim -= y;
+    }
+
+    forward(x) {
+        super.forward(x);
+        this.depth += this.aim * x;
+    }
+}
+
+const submarine2 = new Submarine2();
+readFile('2021/resources/day2.2.txt').split('\n').forEach(line => {
+    const matches = line.matchAll(/^(\w+) (\d+)/g);
+    for (const match of matches) {
+        eval(`submarine2.${match[1]}(${match[2]})`);
+    }
+});
+
+console.info(submarine2.getResult());
